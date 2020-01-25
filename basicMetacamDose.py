b5f9b7bd7dbc2e632ac4f7a73eeacd65af8ba95b
Kris Brunker <krbrunker@gmail.com>
2020-01-25 12:26:29 +0000

    simple calculator that returns a dose of metacam for dogs or cats

---
+++ basicMetacamDose.py
@@ -0,0 +1,12 @@
+>>> breed = input("Is this a dog or a cat?")
+
+weight = float(input("What is the weight?"))
+
+if breed == "dog":
+    dose = weight * 0.04
+    print(dose)
+elif breed == "cat":
+    dose = weight * 0.05
+    print(dose)
+else:
+    print("error")
